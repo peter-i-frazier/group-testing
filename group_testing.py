c2f73b41740e6a141c2c07ad714a646436e4c8b6
masseyc1 <masseycashore@gmail.com>
2020-04-18 04:09:51 -0400

    big rewrite to simulation code, to emphasize the relationship between all important parameters

--- group_testing.py
+++ group_testing.py
@@ -1,6 +1,70 @@
 from math import ceil
-from random import shuffle
+import random
 import numpy as np
+
+class HouseholdGroupTest:
+    def __init__(self, 
+            group_test_size,
+            group_test_participation_rate,
+            false_negative_rate):
+        self.group_test_size = group_test_size
+        self.group_test_participation_rate = group_test_participation_rate
+        self.false_negative_rate = false_negative_rate
+
+    def test(self, population):
+
+        # Make the test groups
+
+        test_individuals = set()
+
+        for (i,j) in population.population - population.fatality_individuals:
+            if (i,j) in population.quarantined_individuals or random.random() < self.group_test_participation_rate:
+                test_individuals.add((i,j))
+
+        number_of_groups = int(ceil(len(test_individuals) / self.group_test_size))
+        ordered_test_individuals = list(test_individuals)
+        random.shuffle(ordered_test_individuals)
+        
+        test_groups = {idx:set() for idx in range(number_of_groups)}
+        group_idx_counter = 0
+        already_grouped_individuals = set()
+
+        for (i,j) in ordered_test_individuals:
+
+            group_idx = group_idx_counter % number_of_groups
+            group_idx_counter += 1
+            
+            if (i,j) not in already_grouped_individuals:
+
+                test_groups[group_idx].add((i,j))
+                already_grouped_individuals.add((i,j))
+
+                for j_new in range(population.household_size):
+                    if (i,j_new) in test_individuals and j_new != j:
+                        assert((i,j_new) not in already_grouped_individuals)
+                        test_groups[group_idx].add((i,j_new))
+                        already_grouped_individuals.add((i,j_new))
+        
+        # Test the groups
+        #import pdb ; pdb.set_trace() 
+        test_results = {}
+
+        for group_idx in test_groups:
+            num_in_group_infected = len([(i,j) for (i,j) in test_groups[group_idx] 
+                                    if (i,j) in population.infected_individuals])
+            if num_in_group_infected == 0:
+                test_detected_presence = False
+            else:
+                false_negative_pct = self.false_negative_rate ** num_in_group_infected
+                if random.random() < false_negative_pct:
+                    test_detected_presence = False
+                else:
+                    test_detected_presence = True
+
+            for (i,j) in test_groups[group_idx]:
+                test_results[(i,j)] = test_detected_presence
+
+        return test_results, number_of_groups
 
 class EmptyTest:
     def __init__(self, infection_pct_blf=0.01):
@@ -55,45 +119,4 @@ class GollierGroupTest:
         
         return test_results, groups, grp_test_data 
 
-class GollierHouseholdGroupTest:
-    def __init__(self, infection_pct_blf=0.01, FNR=0):
-        self.infection_pct_blf=0.01
-
-    def update_infection_blf(self, infection_blf):
-        self.infection_pct_blf = infection_blf
-
-    def test(self, population):
-        n_households = population.get_num_households()
-        avg_house_size = population.get_avg_household_size()
-        # unjustified heuristic to estimate household size
-        household_infection_blf = 1 - (1 - self.infection_pct_blf)** avg_house_size
-        grp_size = -1/np.log(1-household_infection_blf)
-
-        num_grps = int(ceil(n_households / grp_size))
-        
-        groups = {}
-        for grp_num in range(num_grps):
-            groups[grp_num] = []
-
-        households = list(population.iter_households())
-        shuffle(households)
-
-
-        for household, counter in zip(households, range(len(households))):
-            grp = counter % num_grps
-            for individual in population.iter_household_individuals(household):
-                groups[grp].append(individual)
-
-        test_results = {}
-        num_negative_grps = 0
-        for group_idx, grp_individuals in groups.items():
-            result = population.test_group(grp_individuals)
-            test_results[group_idx] = result
-            if not result:
-                num_negative_grps += 1
-
-        grp_test_data = {'num_grps':num_grps, 'grp_size': grp_size,
-                        'num_negative_grps': num_negative_grps}
-        
-        return test_results, groups, grp_test_data 
 
--- population.py
+++ population.py
@@ -1,22 +1,21 @@
 import numpy as np
+import random
+from math import log
 
 class Population:
 
     """ This class describes a population of people and their infection status, optionally including information about their
     household organization and the ability to simulate infection status in an correlated way across households.
     It has methods for simulating infection status forward in time """
-
-    def __init__(self, n, prevalence):
-        # Create a population with n individuals in which the household sizes are all 1 with the given initial disease
-        # prevalence
-
-        self.infectious = np.random.rand(n) < prevalence[0]  # True if infectious
-        self.quarantined = [False]*n    # True if quarantined
-
-        # True if susceptible to the disease, i.e., has not died or developed immunity, and not currently infected
-        self.susceptible = ~self.infectious
-
-    def __init__(self, n_households, household_size_dist, prevalence, SAR, R0, R0_social_dist, d0, fatality_pct, FNR=0):
+    
+    def __init__(self, n_households, 
+                    household_size, 
+                    initial_prevalence, 
+                    disease_length,
+                    non_quarantine_alpha,
+                    daily_secondary_attack_rate,
+                    fatality_pct
+                   ):
         # Initialize a population with non-trivial households
         # n_households:         the number of households in the population
         # household_size_dist:  a numpy array that should sum to 1, where household_size_dist[i] gives the fraction of
@@ -26,246 +25,102 @@ class Population:
         #                       forward in time; SAR is defined as the probability that an infection occurs among
         #                       susceptible people within a household related to a confirmed case
         #
-        assert np.isclose(np.sum(household_size_dist), 1.)
 
-        self.infectious = {}
-        self.susceptible = {}
-        self.prevalence = prevalence
         self.n_households = n_households
-        self.R0 = R0
-        self.R0_social_dist = R0_social_dist
-        self.d0 = d0
-        self.SAR = SAR
+        self.household_size = household_size
+        self.initial_prevalence = initial_prevalence
+        self.daily_secondary_attack_rate = daily_secondary_attack_rate
+        self.non_quarantine_alpha = non_quarantine_alpha
+        self.disease_length = disease_length
         self.fatality_pct = fatality_pct
 
-        self.FNR= FNR
-
-        self.remaining_days_infected = {}
-        self.deaths = set([])
-        self.recoveries = set([])
-        self.active_cases = set([])
-        self.quarantined_households = set([])
-        self.quarantined_individuals = set()
-
-        self.socially_distant_households = set()
-        self.socially_distant_individuals = set()
-
-        self.total_pop = 0
-
-
-        for i in range(n_households):
-            # generate household size h from household_size_dist
-            randomizer = np.random.uniform()
-            h = 0
-            while randomizer >= 0:
-                randomizer -= household_size_dist[h]
-                h += 1
-            self.total_pop += h
-            # compute primary case probability = p*h/(1+SAR*(h-1))
-            prob_prim = prevalence*h/(1+SAR*(h-1))
-            infectious = [np.random.uniform() < prob_prim]
-            if h > 1:
-                # if there are >1 members in the household, and there is a primary case,
-                # generate secondary cases from Bin(h-1, SAR); otherwise, set everyone to be uninfected
-                infectious.extend(np.random.binomial(1, SAR, h-1) * infectious[0] == True)
-            
-            susceptible = [not infected for infected in infectious]
-            
-            self.susceptible[i]=(np.array(susceptible))
-            self.infectious[i] =(np.array(infectious))
-
-        self.update_infection_days()
-
-    def begin_social_distancing(self):
-        for i in range(self.n_households):
-            self.socially_distant_households.add(i)
-            for j in range(len(self.infectious[i])):
-                self.socially_distant_individuals.add((i,j))
-
-    def end_social_distancing(self):
-        for i in range(self.n_households):
-            self.socially_distant_households.discard(i)
-            for j in range(len(self.infectious[i])):
-                self.socially_distant_individuals.discard((i,j))
-
-    def un_distance_household(self, i):
-        self.socially_distant_households.discard(i)
-        for j in range(len(self.infectious[i])):
-            self.socially_distant_individuals.discard((i,j))
-
-    def distance_household(self, i):
-        self.socially_distant_households.discard(i)
-        for j in range(len(self.infectious[i])):
-            self.socially_distant_individuals.discard((i,j))
-    
-    def get_num_social_dist_households(self):
-        return len(self.socially_distant_households)
-
-    def get_num_social_dist(self):
-        return len(self.socially_distant_individuals)
-
-
-    def set_FNR(self, FNR):
-        self.FNR=FNR
-
-    def update_infection_days(self):
-        for i in range(self.n_households):
-            for j in range(len(self.infectious[i])):
-                if self.infectious[i][j]:
-                    if (i,j) not in self.remaining_days_infected:
-                        self.remaining_days_infected[(i,j)] = self.d0
-                        self.active_cases.add((i,j))
-                    elif self.remaining_days_infected[(i,j)] > 0:
-                        self.remaining_days_infected[(i,j)] -= 1
-                        if self.remaining_days_infected[(i,j)] == 0:
-                            if np.random.uniform() < self.fatality_pct:
-                                self.deaths.add((i,j))
-                            else:
-                                self.recoveries.add((i,j))
-                            self.active_cases.discard((i,j))
-                            self.susceptible[i][j] = False
-                            self.infectious[i][j] = False
+        self.population = set([(i,j) for i in range(n_households) for j in range(household_size)]) 
+        self.households = set([i for i in range(n_households)])
 
 
 
-    def __check_indices(self,x):
-        # Make sure that a passed set of individual array indices are valid for our population
-        # TODO: This should be adapted for households?
-        assert(max(x) < self.n)
-        assert(min(x) >= 0)
+        self.quarantined_individuals = set([(i,j) for i in range(n_households) for j in range(household_size)])
+        self.unquarantined_individuals = set()
 
-    def infected(self, x):
-        # Given a set of indices x, return the infection status of those individuals
-        # Used by tests based on this class
-        self.__check_indices(x)
-        return self.infectious[x]
+        self.fatality_individuals = set()
+        self.recovered_individuals = set()
 
-    def get_prevalence(self):
-        # Get current prevalence = (number of infectious unquarantined) / (number of unquarantined)
-        infectious_unquarantined = len(self.active_cases - self.quarantined_individuals)
-        total_unquarantined = self.total_pop - len(self.quarantined_individuals)
+        self.days_infected_so_far = {(i,j):0 for (i,j) in self.population}
+        self.infected_individuals = set([])
 
-        if total_unquarantined == 0:
-            return 0
-        else:
-            return infectious_unquarantined / total_unquarantined
+        self.cumulative_infected_individuals = set()
+
+
+        # infect initial population
+        for (i,j) in self.population:
+            if random.random() < self.initial_prevalence:
+                self.infected_individuals.add((i,j))
+                self.cumulative_infected_individuals.add((i,j))
 
     def step(self):
         # Simulate one step forward in time
         # Simulate how infectious individuals infect each other
         # Unquarantined susceptible people become infected w/ probability = alpha*current prevalence
-        prevalence = self.get_prevalence()
-        for i in range(self.n_households):
-            household_infected = any(self.infectious[i])
-            household_socially_distant = (i in self.socially_distant_households)
 
-            for j in range(len(self.infectious[i])):
-                if household_infected and self.susceptible[i][j]:
-                    secondary_prob = self.SAR # maybe should depend on total # of infected in household, but this is a start
-                    new_secondary = np.random.uniform() < secondary_prob
+
+        # First simulate new primary cases
+        current_prevalence = self.get_current_prevalence()
+        
+        probability_new_infection = log(self.non_quarantine_alpha) * current_prevalence
+
+        for (i,j) in self.unquarantined_individuals:
+            if (i,j) not in self.cumulative_infected_individuals:
+                if random.random() < probability_new_infection:
+                    self.infected_individuals.add((i,j))
+                    self.cumulative_infected_individuals.add((i,j))
+
+        # Next simulate secondary cases
+        for i in self.households:
+            if any([(i,j) in self.infected_individuals for j in range(self.household_size)]):
+                for j in range(self.household_size): 
+                    if (i,j) not in self.cumulative_infected_individuals:
+                        if random.random() < self.daily_secondary_attack_rate:
+                            self.infected_individuals.add((i,j))
+                            self.cumulative_infected_individuals.add((i,j))
+
+        individuals_to_resolve = set()
+        # update infection counts
+        for (i,j) in self.infected_individuals:
+            self.days_infected_so_far[(i,j)] += 1
+
+            # see if the disease has lasted its course 
+            if self.days_infected_so_far[(i,j)] >= self.disease_length:
+                individuals_to_resolve.add((i,j))
+
+        for (i,j) in individuals_to_resolve:
+
+                self.infected_individuals.discard((i,j)) 
+
+                if random.random() < self.fatality_pct:
+                    self.fatality_individuals.add((i,j))
                 else:
-                    new_secondary = False
+                    self.recovered_individuals.add((i,j))
+   
 
-                if not (i,j) in self.quarantined_individuals and self.susceptible[i][j]:
-                    if household_socially_distant:
-                        primary_prob = np.log(self.R0) * prevalence / self.d0
-                    else:
-                        primary_prob = np.log(self.R0) * prevalence / self.d0
-                    new_primary = np.random.uniform() < primary_prob
-                else:
-                    new_primary = False
+    def get_current_prevalence(self):
+        total_unquarantined_infected = len(self.infected_individuals.intersection(self.unquarantined_individuals))
+        total_unquarantined = len(self.unquarantined_individuals)
 
-                self.infectious[i][j] = new_primary or new_secondary or self.infectious[i][j]
-
-        self.update_infection_days()
-
-    def get_population_size(self):
-        return self.total_pop
-    
-    def get_num_households(self):
-        return self.n_households
-
-    def get_avg_household_size(self):
-        return np.mean([len(household) for household in self.infectious.values()])
-    
-    def iter_individuals(self):
-        for i in range(self.n_households):
-            for j in range(len(self.infectious[i])):
-                yield (i,j)
-
-    def iter_households(self):
-        for i in range(self.n_households):
-            yield i
-
-    def iter_household_individuals(self, household):
-        for j in range(len(self.infectious[household])):
-            yield (household, j)
-
-    def test_group(self, grp_individuals):
-        infected_subgrp = self.active_cases.intersection(set(grp_individuals))
-        if len(infected_subgrp) == 0:
-            return False
+        if total_unquarantined == 0:
+            return 0
         else:
-            false_neg_prob = self.FNR ** len(infected_subgrp)
-            if np.random.uniform() < false_neg_prob:
-                return False
-            else:
-                return True
+            return total_unquarantined_infected / total_unquarantined
 
-    def any_infectious(self, grp_individuals):
-        return any([self.infectious[i][j] for (i,j) in grp_individuals])
-
-    def react_to_test(self, test_results, groups):
-        # unquarantine all confirmed negative results who do not also have
-        # an infected family member
-        household_risk_status = [False] * self.n_households
-        for group_idx, result in test_results.items():
-            # for any positive group, set all household risk statuses to True
-            # within that group
-            if result:
-                for i,_ in groups[group_idx]:
-                    household_risk_status[i] = True
-
-        # unquarantine all households with negative risk status
-        for i, risk_status in enumerate(household_risk_status):
-            if risk_status:
-                self.quarantine_household(i)
-            else:
-                self.unquarantine_household(i)
-
+   
     def quarantine_household(self, i):
-        for j in range(len(self.infectious[i])):
+        for j in range(self.household_size):
+            self.unquarantined_individuals.discard((i,j))
             self.quarantined_individuals.add((i,j))
-        self.quarantined_households.add(i)
+
 
     def unquarantine_household(self, i):
-        for j in range(len(self.infectious[i])):
+        for j in range(self.household_size):
             self.quarantined_individuals.discard((i,j))
-        self.quarantined_households.discard(i)
+            self.unquarantined_individuals.add((i,j))
 
-
-    def quarantine(self, i, j):
-        # Put into quarantine all individuals whose indices are in the list x
-        if (i,j) not in self.deaths:
-            self.quarantined[i][j] = True
-
-    def unquarantine(self, i, j):
-        # Remove from quarantine all individuals whose indices are in the list x
-        self.quarantined[i][j] = False
-
-    def get_num_infected(self):
-        return len(self.active_cases)
-
-    def get_total_dead(self):
-        return len(self.deaths)
-
-    def get_total_recovered(self):
-        return len(self.recoveries)
-
-    def get_num_quarantined(self):
-        return len(self.quarantined_individuals)
-    
-    def get_num_households_quarantined(self):
-        return len(self.quarantined_households)
-    #def num_recovered_dead(self):
-    # for now we haven't taken into consideration the duration of infection and recovery yet
+   
