50a1a4b04c190b9f7753fef1886d09030f8d63cf
masseyc1 <masseycashore@gmail.com>
2020-05-07 18:37:02 -0400

    updates to stochastic population-level sims

--- src/simulations_v2/stochastic_simulation.py
+++ src/simulations_v2/stochastic_simulation.py
@@ -13,18 +13,25 @@ class StochasticSimulation:
         # Meta-parameters governing the maximum number of days an
         # individual spends in each 'infection' state
         self.max_time_E = params['max_time_exposed']
-        self.max_time_pre_ID = params['max_time_pre_ID']
-        self.max_time_ID = params['max_time_ID']
+        self.max_time_pre_ID = params['max_time_pre_ID'] 
+        self.max_time_ID = params['max_time_ID'] 
+        self.max_time_SyID_mild = params['max_time_SyID_mild'] 
+        self.max_time_SyID_severe = params['max_time_SyID_severe'] 
        
         # parameters governing distribution over time spent in each
         # of these infection states:
         # Assumptions about the sample_X_times variables:
-        # sample_X_times(n) returns a numpy array times of length max_time_X
+        # sample_X_times(n) returns a numpy array times of length max_time_X+1
         # such that times[k] is the number of people who stay in state X
-        # for k time periods, and sum(times) == n.
+        # for k time periods. 
+        # (However, the length of the corresponding queue arrays will just be max_time_X,
+        # not max_time_X + 1)
+        # We assume that sum(times) == n
         self.sample_E_times = params['exposed_time_function']
         self.sample_pre_ID_times = params['pre_ID_time_function']
         self.sample_ID_times = params['ID_time_function']
+        self.sample_SyID_mild_times = params['SyID_mild_time_function']
+        self.sample_SyID_severe_times = params['SyID_severe_time_function']
 
         # assumption: sample_QI_exit_count(n) returns a number m <= n
         #             indicating the number of people in the state QI
@@ -36,7 +43,12 @@ class StochasticSimulation:
         # parameters governing distribution over transition out of
         # each infection state
         self.exposed_infection_p = params['exposed_infection_p']
-        self.contacts_lambda = params['expected_contacts_per_day']
+        self.daily_contacts_lambda = params['expected_contacts_per_day']
+        self.mild_symptoms_p = params['mild_symptoms_p']
+
+        # parameters governing symptomatic daily self reporting
+        self.mild_self_report_p = params['mild_symptoms_self_report_p']
+        self.severe_self_report_p = params['severe_symptoms_self_report_p']
 
         # parameters governing test protocol
         self.days_between_tests = params['days_between_tests']
@@ -47,6 +59,7 @@ class StochasticSimulation:
         # parameters governing contact tracing
         self.perform_contact_tracing = params['perform_contact_tracing']
         self.contact_tracing_c = params['contact_tracing_constant']
+        self.contact_tracing_delay = params['contact_tracing_delay']
 
         # flag governing meaning of the pre-ID state
         self.pre_ID_state = params['pre_ID_state']
@@ -57,9 +70,12 @@ class StochasticSimulation:
         self.init_E_count = params['initial_E_count']
         self.init_pre_ID_count = params['initial_pre_ID_count']
         self.init_ID_count = params['initial_ID_count']
+        self.init_SyID_mild_count = params['initial_SyID_mild_count']
+        self.init_SyID_severe_count = params['initial_SyID_severe_count']
 
         self.init_S_count = self.pop_size - self.init_E_count - \
-                        self.init_pre_ID_count - self.init_ID_count
+                        self.init_pre_ID_count - self.init_ID_count - \
+                        self.init_SyID_mild_count - self.init_SyID_severe_count
         assert(self.init_S_count >= 0)
         
         # instantiate state variables and relevant simulation variables
@@ -67,12 +83,35 @@ class StochasticSimulation:
     
     def reset_initial_state(self):
         self.S = self.init_S_count
-        self.E = self.sample_E_times(self.init_E_count)
-        self.pre_ID = self.sample_pre_ID_times(self.init_pre_ID_count)
-        self.ID = self.sample_ID_times(self.init_ID_count)
+
+        # all of the following state vectors have the following convention:
+        # state[k] is how many people have k days left to go.
+        E_sample = self.sample_E_times(self.init_E_count)
+        self.E = E_sample[1:]
+
+        pre_ID_sample = self.sample_pre_ID_times(self.init_pre_ID_count + E_sample[0])
+        self.pre_ID = pre_ID_sample[1:]
+
+        ID_sample = self.sample_ID_times(self.init_ID_count + pre_ID_sample[0])
+        self.ID = ID_sample[1:]
+
+        additional_mild = np.random.sample(ID_sample[0], self.mild_symptoms_p)
+        additional_severe = ID_sample[0] - additional_mild
+
+        SyID_mild_sample = self.sample_SyID_mild_times(self.init_SyID_mild_count + additional_mild)
+        self.SyID_mild = SyID_mild_sample[1:]
+
+        SyID_severe_sample = self.sample_SyID_severe_times(self.init_SyID_severe_count + additional_severe)
+        self.SyID_severe = SyID_severe_sample[1:]
+
+        # contact_trace_queue[k] are the number of quarantined individuals who have k
+        # days remaining until the results from their contact trace comes in
+        self.contact_trace_queue = [0] * (self.contact_tracing_delay + 1)
+
         self.QS = 0
         self.QI = 0
-        self.R = 0
+
+        self.R = SyID_mild_sample[0] + SyID_mild_severe[0]
         
         
         var_labels = self.get_state_vector_labels()
@@ -87,20 +126,32 @@ class StochasticSimulation:
             self.step()
         return self.sim_df
      
-    def run_contact_trace(self, new_QI):
-        leave_E = min(sum(self.E), new_QI * self.contact_tracing_c)
-        new_QI = int(self.exposed_infection_p * leave_E)
-        new_QS = leave_E - new_QI
-        self.QS = self.QS + new_QS
-        self.QI = self.QI + new_QI
+    def step_contact_trace(self, new_QI):
+        """ resolve contact traces at the front of the queue and add new QIs to the back 
+        of the contact trace queue"""
+
+        # update the contact trace queue
+        self.contact_trace_queue[self.contact_tracing_delay] += new_QI
+        resolve_today_QI = self.contact_trace_queue[0]
+        self._shift_contact_queue()
+
+        leave_E = int(min(sum(self.E), resolve_today_QI * self.contact_tracing_c))
+        self.QI = self.QI + leave_E
 
         idx = self.max_time_E - 1
         while leave_E > 0:
-            leave_E_idx = min(self.E[idx], leave_E)
-            self.E[idx] -= leave_E_idx
+            leave_E_at_idx = min(self.E[idx], leave_E)
+            self.E[idx] -= leave_E_at_idx
             leave_E -= leave_E_idx
             idx -= 1
-        
+
+      def _shift_contact_queue(self):
+        idx = 0
+        while idx <= self.contact_tracing_delay - 1:
+            self.contact_trace_queue[idx] = self.contact_trace_queue[idx+1]
+            idx += 1
+        self.contact_trace_queue[self.contact_tracing_delay] = 0
+   
 
     def run_test(self):
         """ execute one step of the testing logic """
@@ -112,99 +163,146 @@ class StochasticSimulation:
         new_QI_p = self.test_pop_fraction * (1 - self.test_QFNR)
 
         # sample the number of free infected people who end up quarantined
-        # first from the exposed state -- multiply by exposed_infection_p to account for uncertain
-        # nature of infection status in the E group
-        new_QI_from_E = np.random.binomial(self.E, self.exposed_infection_p * new_QI_p)
         new_QI_from_ID = np.random.binomial(self.ID, new_QI_p)
+        new_QI_from_SyID_mild = np.random.binomial(self.SyID_mild, new_QI_p)
+        new_QI_from_SyID_severe = np.random.binomial(self.SyID_severe, new_QI_p)
 
+        # update counts in relevant states
+        self.ID = self.ID - new_QI_from_ID
+        self.SyID_mild = self.SyID_mild - new_QI_from_SyID_mild
+        self.SyID_severe = self.SyID_severe - new_QI_from_SyID_severe
+
+        new_QI = sum(new_QI_from_ID) + sum(new_QI_from_SyID_mild) + sum(new_QI_from_SyID_severe)
+
+        # do the above for pre-ID state, if it is detectable
         if self.pre_ID_state == 'detectable':
             new_QI_from_pre_ID = np.random.binomial(self.pre_ID, new_QI_p)
-        else:
-            # if pre-ID state is not detectable, we can still get lucky via the QFPR
-            pre_ID_quarantine_p = self.test_pop_fraction * self.QFPR
-            new_QI_from_pre_ID = np.random.binomial(self.pre_ID, pre_ID_quarantine_p)
+            self.pre_ID = self.pre_ID - new_QI_from_pre_ID
+            new_QI += sum(new_QI_from_pre_ID)
 
-        # probability a free-susceptible person becomes quarantined
+        # add to QI individuals from E, and from pre-ID (if state is 'infectious'), using
+        # the false-positive rate for undetectable individuals
+        new_QI_undetectable_p = self.test_pop_fraction * self.test_QFPR
+
+        new_QI_from_E = np.random.binomial(self.E, new_QI_undetectable_p)
+        self.E = self.E - new_QI_from_E
+        new_QI += sum(new_QI_from_E)
+
+        if self.pre_ID_state == 'infectious':
+            new_QI_from_pre_ID = np.random.binomial(self.pre_ID, new_QI_undetectable_p)
+            self.pre_ID = self.pre_ID - new_QI_from_pre_ID
+            new_QI += sum(new_QI_from_pre_ID)
+
+        # add to QS individuals from S, due to false positives
         new_QS_p = self.test_pop_fraction *  self.test_QFPR
         # sample number of free susceptible people who become quarantined
         new_QS_from_S = np.random.binomial(self.S, new_QS_p)
-        new_QS_from_E = np.random.binomial(self.E, (1 - self.exposed_infection_p) * new_QS_p)
-
-        # decrease new_QS_from_E so we don't over-pull from the E queue
-        new_QS_from_E = np.minimum(new_QS_from_E, self.E - new_QI_from_E)
-
-        # use above samples to update the state variables:
-        self.E = self.E - new_QI_from_E - new_QS_from_E
-        assert(min(self.E) >= 0)
-        self.pre_ID = self.pre_ID - new_QI_from_pre_ID
-        self.ID = self.ID - new_QI_from_ID
         self.S = self.S - new_QS_from_S
 
-        new_QI = sum(new_QI_from_E) + sum(new_QI_from_pre_ID) + sum(new_QI_from_ID)
-        self.QI = self.QI + new_QI   
+        # update QS and QI
+        self.QS = self.QS + new_QS_from_S
+        self.QI = self.QI + new_QI
 
-        new_QS = new_QS_from_S + sum(new_QS_from_E)
-        self.QS = self.QS + new_QS
+        return new_QI
 
-        if self.perform_contact_tracing:
-            self.run_contact_trace(new_QI)
+
+    def isolate_self_reports(self):
+        mild_self_reports = np.random.binomial(self.SyID_mild, self.mild_self_report_p)
+        self.SyID_mild = self.SyID_mild - mild_self_reports
+        new_QI = sum(mild_self_reports)
+
+        severe_self_reports = np.random.binomial(self.SyID_severe, self.severe_self_report_p)
+        self.SyID_severe = self.SyID_severe - severe_self_reports
+        new_QI += sum(severe_self_reports)
+
+        self.QI = self.QI + new_QI
+
+        return new_QI
+        
 
 
     def step(self):
         """ simulate a single day in the progression of the disease """
 
+        new_QI = 0
         # do testing logic first 
         if self.current_day - self.last_test_day >= self.days_between_tests or self.last_test_day == -1:
             self.last_test_day = self.current_day
-            self.run_test() 
+            new_QI += self.run_test() 
 
+        # resolve symptomatic self-reporting
+        new_QI += self.isolate_self_reports()
+
+        # do contact tracing
+        if self.perform_contact_tracing:
+            self.step_contact_trace(new_QI)
+
+        # simulate number of contacts between free infectious & free susceptible:
         free_infectious = 0
+
         if self.pre_ID_state == 'infectious':
             free_infectious += sum(self.pre_ID)
-        free_infectious += sum(self.ID)
-        # free_infectious += sum(self.E) * self.exposed_infection_p
+
+        free_infectious += sum(self.ID) + sum(self.SyID_mild) + sum(self.SyID_severe)
 
         free_susceptible = self.S + (1 - self.exposed_infection_p) * sum(self.E)
-
-
-        # simulate new exposures between free infectious & free susceptible:
         free_tot = free_infectious + free_susceptible + self.R + sum(self.E) * self.exposed_infection_p
+
         if self.pre_ID_state == 'detectable':
             free_tot += sum(self.pre_ID)
 
-        poisson_param = free_infectious * self.contacts_lambda * free_susceptible / free_tot
+        poisson_param = free_infectious * self.daily_contacts_lambda * free_susceptible / free_tot
+        n_contacts = np.random.poisson(poisson_param)
 
-        new_E = min(np.random.poisson(poisson_param), self.S)
+        # sample number of new E cases
+        new_E = min(np.random.binomial(n_contacts, self.exposed_infection_p), self.S)
+        self.S -= new_E
 
-        # resolve exposures queue
-        new_S = np.random.binomial(self.E[0], 1 - self.exposed_infection_p)
-        new_pre_ID = self.E[0] - new_S
+        # update E queue and record new pre-ID cases
+        new_E_times = self.sample_E_times(new_E)
+        new_pre_ID = self.E[0] + new_E_times[0]
+        self._shift_E_queue()
+        self.E = self.E + new_E_times[1:]
 
-        # resolve pre-ID queue
-        new_ID = self.pre_ID[0]
-        
-        # resolve ID queue
-        new_R = self.ID[0]
+        # sample times of new pre-ID cases / update pre-ID queue/ record new ID cases
+        new_pre_ID_times = self.sample_pre_ID_times(new_pre_ID)
+        new_ID = self.pre_ID[0] + new_pre_ID_times[0]
+        self._shift_pre_ID_queue()
+        self.pre_ID = self.pre_ID + new_pre_ID_times[1:]
 
-        # sample number of people who leave quarantine
+
+        # sample times of new ID cases / update ID queue/ record new SyID cases
+        new_ID_times = self.sample_ID_times(new_ID)
+        new_SyID = self.ID[0] + new_ID_times[0]
+        self._shift_ID_queue()
+        self.ID = self.ID + new_ID_times[1:]
+
+        # decompose new_SyID into mild and severe
+        new_SyID_mild = np.random.binomial(new_SyID, self.mild_symptoms_p)
+        new_SyID_severe = new_SyID - new_SyID_mild
+
+        # samples times of new SyID mild cases/ update mild queue/ record new R cases
+        new_SyID_mild_times = self.sample_SyID_mild_times(new_SyID_mild)
+        new_R_from_mild = self.SyID_mild[0] + new_SyID_mild_times[0]
+        self._shift_SyID_mild_queue()
+        self.SyID_mild = self.SyID_mild + new_SyID_mild_times[1:]
+
+        # same as above, but for the severe symptom queue
+        new_SyID_severe_times = self.sample_SyID_severe_times(new_SyID_severe)
+        new_R_from_severe = self.SyID_severe[0] + new_SyID_severe_times[0]
+        self._shift_SyID_severe_queue()
+        self.SyID_severe = self.SyID_severe + new_SyID_severe_times[1:]
+
+
+        # sample number of people who leave quarantine-I/ resolve new R cases
         leave_QI = self.sample_QI_exit_count(self.QI)
-        new_R += leave_QI
-
-        leave_QS = self.sample_QS_exit_count(self.QS)
-        new_S += leave_QS
-
-        # update relevant state variables:
-        self.S = self.S + new_S - new_E
-        self.R += new_R
-
         self.QI -= leave_QI
-        self.QS -= leave_QS
+        self.R += leave_QI + new_R_from_mild + new_R_from_severe
 
-        # update array-based state variables
-        self._shift_array_state_variables()
-        self.E = self.E + self.sample_E_times(new_E)
-        self.pre_ID = self.pre_ID + self.sample_pre_ID_times(new_pre_ID)
-        self.ID = self.ID + self.sample_ID_times(new_ID)
+        # sample number of people who leave quarantine-S
+        leave_QS = self.sample_QS_exit_count(self.QS)
+        self.QS -= leave_QS
+        self.S += leave_QS
 
         self._append_sim_df()
 
@@ -220,37 +318,55 @@ class StochasticSimulation:
         if sum(data) != self.pop_size:
             raise(Exception("population has shrunk"))
 
-    def _shift_array_state_variables(self):
+    def _shift_E_queue(self):
         idx = 0
         while idx <= self.max_time_E - 2:
             self.E[idx] = self.E[idx+1]
             idx += 1
         self.E[self.max_time_E - 1] = 0
-        
+
+    def _shift_pre_ID_queue(self): 
         idx = 0
         while idx <= self.max_time_pre_ID - 2:
             self.pre_ID[idx] = self.pre_ID[idx+1]
             idx += 1
         self.pre_ID[self.max_time_pre_ID - 1] = 0
 
+    def _shift_ID_queue(self):
         idx = 0
         while idx <= self.max_time_ID - 2:
             self.ID[idx] = self.ID[idx+1]
             idx += 1
         self.ID[self.max_time_ID - 1] = 0
 
+    def _shift_SyID_mild_queue(self):
+        idx = 0
+        while idx <= self.max_time_SyID_mild - 2:
+            self.SyID_mild[idx] = self.SyID_mild[idx+1]
+            idx += 1
+        self.SyID_mild[self.max_time_SyID_mild - 1] = 0
+
+    def _shift_SyID_severe_queue(self):
+        idx = 0
+        while idx <= self.max_time_SyID_mild - 2:
+            self.SyID_severe[idx] = self.SyID_severe[idx+1]
+            idx += 1
+        self.SyID_severe[self.max_time_SyID_severe - 1] = 0
+
 
     def get_current_state_vector(self):
         return np.concatenate([
             [self.S], [self.QS], [self.QI], [self.R],
-            self.E, self.pre_ID, self.ID
+            self.E, self.pre_ID, self.ID, self.SyID_mild, self.SyID_severe
             ])
 
     def get_state_vector_labels(self):
         return ['S', 'QS', 'QI', 'R'] + \
                 ['E_{}'.format(x) for x in range(self.max_time_E)] + \
                 ['pre_ID_{}'.format(x) for x in range(self.max_time_pre_ID)] + \
-                ['ID_{}'.format(x) for x in range(self.max_time_ID)] 
+                ['ID_{}'.format(x) for x in range(self.max_time_ID)] + \
+                ['SyID_mild_{}'.format(x) for x in range(self.max_time_SyID_mild)] + \
+                ['SyID_severe_{}'.format(x) for x in range(self.max_time_SyID_severe)]
 
 
 
