25f9937ca2d13d60cb3794202451c51a2ae27a21
masseyc1 <masseycashore@gmail.com>
2020-08-04 10:49:55 -0400

    add single-dorm adaptive-testing simulation code & notebooks

--- src/simulations_v2/single_dorm_discrete_test_schedule.py
+++ src/simulations_v2/single_dorm_discrete_test_schedule.py
@@ -12,10 +12,14 @@ class SingleDormSimulation(StochasticSimulation):
             initial_cases=1,
             safe_days_to_exit_high_alert=3,
             use_default_outside_infection_p=False,
-            config=None,
+            base_test_schedule=(1, 3),
+            high_alert_test_schedule=(1,3,5),
             base_config="/home/jmc678/covid_data/group-testing/src/simulations_v2/"
             "params/june8params/noreopen/nominal_students.yaml"):
         
+        self.base_test_schedule=base_test_schedule
+        self.high_alert_test_schedule = high_alert_test_schedule
+
         self.dorm_test_rate = dorm_test_rate
         self.high_alert_dorm_test_rate = high_alert_dorm_test_rate
 
@@ -34,10 +38,6 @@ class SingleDormSimulation(StochasticSimulation):
         base_params['initial_ID_count'] = initial_cases
         if not use_default_outside_infection_p:
             base_params['daily_outside_infection_p'] = 0
-
-        if config != None:
-            for key, val in config.items():
-                base_params[key] = val
 
         super(SingleDormSimulation, self).__init__(base_params)
 
@@ -63,15 +63,17 @@ class SingleDormSimulation(StochasticSimulation):
     
 
     def update_days_since_last_positive(self):
-        if self.new_QI_from_self_reports > 0 or self.new_QI_from_last_test > 0:
-            self.days_since_last_positive = 0
-        else:
-            self.days_since_last_positive += 1
+        if self.test_pop_fraction != 0: # hack to check if today is a test-day or not
+            if self.new_QI_from_self_reports > 0 or self.new_QI_from_last_test > 0:
+                self.days_since_last_positive = 0
+            else:
+                self.days_since_last_positive += 1
 
 
-    def update_high_alert_mode(self):
-        self.update_days_since_last_positive()
-        if self.days_since_last_positive == 0 and not self.high_alert_mode:
+    def update_high_alert_mode(self, force_on=False):
+        if not force_on:
+            self.update_days_since_last_positive()
+        if force_on or (self.days_since_last_positive == 0 and not self.high_alert_mode):
             self.high_alert_mode = True
             self.test_pop_fraction = self.high_alert_dorm_test_rate
             self.daily_contacts_lambda = self.high_alert_contacts_per_day
@@ -81,12 +83,27 @@ class SingleDormSimulation(StochasticSimulation):
             self.daily_contacts_lambda = self.contacts_per_day
 
 
+    def update_test_parameters(self, day_number):
+        if self.high_alert_mode:
+            test_schedule = self.high_alert_test_schedule
+            test_rate = self.high_alert_dorm_test_rate
+        else:
+            test_schedule = self.base_test_schedule
+            test_rate = self.dorm_test_rate
+
+        if day_number % 7 in test_schedule:
+            self.test_pop_fraction = test_rate
+        else:
+            self.test_pop_fraction = 0
+
+
     def run_new_trajectory(self, max_days=112):
         
         days_controlled = []
         free_infected_counts = []
         high_alert_statuses = []
         while len(free_infected_counts) <= max_days:
+            self.update_test_parameters(len(free_infected_counts))
             self.step()
             self.update_high_alert_mode()
 
