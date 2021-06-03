9447ac767869270feef8975232a043f48b06b67d
alyfjanmohamed <aaj54@whale.orie.cornell.edu>
2021-06-03 15:06:30 -0400

    plotting config file

---
+++ notebooks/pnas_paper_figs/plot_utils.py
@@ -0,0 +1,8 @@
+def configure_plot(plt):
+    plt.style.use('seaborn-white')
+    plt.rcParams['axes.titlesize'] = 20
+    plt.rcParams['axes.labelsize'] = 16
+    plt.rcParams['legend.fontsize'] = 14
+    plt.rcParams['xtick.labelsize'] = 14
+    plt.rcParams['ytick.labelsize'] = 14
+
