e2d4d3a22c1611786eb1b23137f775ac49e8c428
masseyc1 <masseycashore@gmail.com>
2021-06-09 12:09:02 -0400

    add changes to run_sensitivity_analyses script and updated shutdown_sims notebooks

--- notebooks/pnas_paper_figs/run_sensitivity_analyses.py
+++ notebooks/pnas_paper_figs/run_sensitivity_analyses.py
@@ -14,8 +14,8 @@ from sim_helper_functions import *
 
 
 def launch_sensitivity_analysis(centre, pess, param, base_folder, nreps=50):
-    param_lb = min(centre[param], pess[param])
-    param_ub = max(centre[param], pess[param])
+    param_lb = PARAM_BOUNDS[param][0]
+    param_ub = PARAM_BOUNDS[param][1]
 
     centre_lb = centre.copy()
     centre_lb[param] = param_lb
@@ -23,14 +23,15 @@ def launch_sensitivity_analysis(centre, pess, param, base_folder, nreps=50):
     centre_ub = centre.copy()
     centre_ub[param] = param_ub
 
-    centre_points = get_points_on_line(centre_lb, centre_ub, mult_lb=-1.1, mult_ub=1.1)
+    centre_points = get_points_on_line(centre_lb, centre_ub, mult_lb=0, mult_ub=1)
 
     pess_lb = pess.copy()
     pess_lb[param] = param_lb
     pess_ub = pess.copy()
     pess_ub[param] = param_ub
 
-    pess_points = get_points_on_line(pess_lb, pess_ub, mult_lb=-1.1, mult_ub=1.1)
+    pess_points = get_points_on_line(pess_lb, pess_ub, mult_lb=0, mult_ub=1)
+
 
     folder_name = '{}/{}/'.format(base_folder, param)
     os.mkdir(folder_name)
@@ -43,7 +44,6 @@ def launch_sensitivity_analysis(centre, pess, param, base_folder, nreps=50):
 
     uncertainty_points = centre_points + pess_points
     fnames = centre_fnames + pess_fnames
-    #import pdb; pdb.set_trace()
     processes = run_sims_new_process(uncertainty_points, fnames, nreps=nreps, run_only_residential=False,
             wait_for_processes_to_join=False)
     return processes
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     res_pessimistic = calculate_pessimistic_scenario(res_results)
     centre = get_centre_point()
 
-    base_folder = './may_29_sims/sensitivity_sims_{}/'.format(get_timestamp())
+    base_folder = './may_31_sims/sensitivity_sims_{}/'.format(get_timestamp())
     os.mkdir(base_folder)
 
     processes = []
