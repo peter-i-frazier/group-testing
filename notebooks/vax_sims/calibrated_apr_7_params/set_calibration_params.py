d80c622087316d778de3a41fa3df4cdd24b16ebb
masseyc1 <masseycashore@gmail.com>
2021-07-30 16:52:32 -0400

    add code and starter notebooks for vaccination simulations

---
+++ notebooks/vax_sims/calibrated_apr_7_params/set_calibration_params.py
@@ -0,0 +1,112 @@
+import yaml
+
+with open('nominal.yaml') as f:
+    nominal_params = yaml.load(f, Loader=yaml.FullLoader)
+
+"""
+parameter update history:
+[x] expected_contacts_per_day updated 4/7
+[x] cases_isolated_per_contact -- students: updated 1/11
+[x] cases_quarantined_per_contact updated 4/7
+[x] cases_isolated_per_contact -- faculty+staff: updated 1/12
+[x] test sensitivity updated 4/7
+
+
+private calibration for students
+group 1
+"""
+params_group_1_pre_semester = nominal_params.copy()
+params_group_1_pre_semester['population_size'] = 3533
+params_group_1_pre_semester['test_population_fraction'] = 0.0255
+params_group_1_pre_semester['expected_contacts_per_day'] = 0.736
+params_group_1_pre_semester['cases_isolated_per_contact'] = 1.329
+params_group_1_pre_semester['cases_quarantined_per_contact'] = 4.785
+params_group_1_pre_semester['initial_ID_prevalence'] = 0
+params_group_1_pre_semester['test_protocol_QFNR'] = 1 - 0.6 * 0.974 # 0.4156
+params_group_1_pre_semester['daily_outside_infection_p'] = 1.42E-5
+params_group_1_pre_semester['_scenario_name'] = 'Group 1 Students (pre-semester) Parameters, Private'
+
+with open('group_1_students_pre_semester_private.yaml', 'w') as f:
+    yaml.dump(params_group_1_pre_semester, f)
+
+params_group_1_post_movein_private = params_group_1_pre_semester.copy()
+params_group_1_post_movein_private['population_size'] = 0
+params_group_1_post_movein_private['test_population_fraction'] = 0.285714
+params_group_1_post_movein_private['_scenario_name'] = 'Group 1 Students (post move-in) Parameters, Private'
+
+with open('group_1_students_post_movein_private.yaml', 'w') as f:
+    yaml.dump(params_group_1_post_movein_private, f)
+
+
+"""
+private calibration for students
+group 2
+"""
+params_group_2_pre_semester = params_group_1_pre_semester.copy()
+params_group_2_pre_semester['population_size'] = 8434
+params_group_2_pre_semester['test_population_fraction'] = 0.0255
+params_group_2_pre_semester['expected_contacts_per_day'] = 0.148
+params_group_2_pre_semester['initial_ID_prevalence'] = 0
+params_group_2_pre_semester['daily_outside_infection_p'] = 7.11E-6
+params_group_2_pre_semester['_scenario_name'] = 'Group 2 Students (pre-semester) Parameters, Private'
+
+with open('group_2_students_pre_semester_private.yaml', 'w') as f:
+    yaml.dump(params_group_2_pre_semester, f)
+
+params_group_2_post_movein_private = params_group_2_pre_semester.copy()
+params_group_2_post_movein_private['population_size'] = 0
+params_group_2_post_movein_private['test_population_fraction'] = 0.285714
+params_group_2_post_movein_private['_scenario_name'] = 'Group 2 Students (post move-in) Parameters, Private'
+
+with open('group_2_students_post_movein_private.yaml', 'w') as f:
+    yaml.dump(params_group_2_post_movein_private, f)
+
+
+"""
+private calibration for students
+group 3
+"""
+params_group_3_pre_semester = params_group_1_pre_semester.copy()
+params_group_3_pre_semester['population_size'] = 6202
+params_group_3_pre_semester['test_population_fraction'] = 0
+params_group_3_pre_semester['expected_contacts_per_day'] = 0.067
+params_group_3_pre_semester['initial_ID_prevalence'] = 0
+params_group_3_pre_semester['daily_outside_infection_p'] = 6.45E-6
+params_group_3_pre_semester['_scenario_name'] = 'Group 3 Students (pre-semester) Parameters, Private'
+
+with open('group_3_students_pre_semester_private.yaml', 'w') as f:
+    yaml.dump(params_group_3_pre_semester, f)
+
+params_group_3_post_movein_private = params_group_3_pre_semester.copy()
+params_group_3_post_movein_private['population_size'] = 0
+params_group_3_post_movein_private['test_population_fraction'] = 0.142857
+params_group_3_post_movein_private['_scenario_name'] = 'Group 3 Students (post move-in) Parameters, Private'
+
+with open('group_3_students_post_movein_private.yaml', 'w') as f:
+    yaml.dump(params_group_3_post_movein_private, f)
+
+
+
+"""
+private calibration for faculty + staff
+"""
+params_faculty_staff_pre_semester_private = nominal_params.copy()
+params_faculty_staff_pre_semester_private['population_size'] = 10283
+params_faculty_staff_pre_semester_private['test_population_fraction'] = 0
+params_faculty_staff_pre_semester_private['expected_contacts_per_day'] = 10
+params_faculty_staff_pre_semester_private['initial_ID_prevalence'] = 0
+params_faculty_staff_pre_semester_private['test_protocol_QFNR'] = 1 - 0.6 # 0.4
+params_faculty_staff_pre_semester_private['cases_isolated_per_contact'] = 0.255
+params_faculty_staff_pre_semester_private['daily_outside_infection_p'] = 0
+params_faculty_staff_pre_semester_private['_scenario_name'] = 'Faculty + Staff (pre-semester) Parameters, Private'
+
+with open('faculty_staff_pre_semester_private.yaml', 'w') as f:
+    yaml.dump(params_faculty_staff_pre_semester_private, f)
+
+params_faculty_staff_post_movein_private = params_faculty_staff_pre_semester_private.copy()
+params_faculty_staff_post_movein_private['population_size'] = 0
+params_faculty_staff_post_movein_private['test_population_fraction'] = 12.74/130
+params_faculty_staff_post_movein_private['_scenario_name'] = 'Faculty + Staff (post move-in) Parameters, Private'
+
+with open('faculty_staff_post_movein_private.yaml', 'w') as f:
+    yaml.dump(params_faculty_staff_post_movein_private, f)
--- src/simulations_v2/vax_sim_utils.py
+++ src/simulations_v2/vax_sim_utils.py
@@ -1,5 +1,7 @@
 from multi_group_simulation import MultiGroupSimulation
 import numpy as np
+import yaml
+from load_params import load_params
 
 def create_multigrp_vax_sim(group_params, group_vax_statuses, interaction_matrix, 
                                 vax_trans_mult, vax_susc_mult):
@@ -20,3 +22,40 @@ def create_multigrp_vax_sim(group_params, group_vax_statuses, interaction_matrix
             vax_weighted_interaction_matrix[i,j] = j_to_i_contact_freq
 
     return MultiGroupSimulation(group_params, vax_weighted_interaction_matrix)
+
+def load_vax_group_configs(vax_config_yaml):
+    with open(vax_config_yaml, "rb") as f:
+        vax_config = yaml.load(f)
+
+    base_config_yaml = vax_config['_base_config']
+    base_config_unvax = load_params(base_config_yaml)[1]
+
+    base_config_vax = base_config_unvax.copy()
+
+    base_config_unvax['test_population_fraction'] = vax_config['unvax_test_freq']
+    base_config_vax['test_population_fraction'] = vax_config['vax_test_freq']
+
+    popsize = base_config_unvax['population_size']
+
+    vax_popsize = int(popsize * vax_config['vax_proportion'])
+    unvax_popsize = popsize - vax_popsize
+
+    base_config_vax['population_size'] = vax_popsize
+    base_config_unvax['population_size'] = unvax_popsize
+
+    default_contacts = base_config_unvax['expected_contacts_per_day']
+
+    # 0 corresponds to vax group, 1 corresponds to unvax
+    # contact_matrix[i,j] means how many times an individual in j coughs on an individual in i
+    # contact_matrix[i,0] = default_contacts * (vax_population / total_population)
+    #
+    contact_matrix = np.zeros((2,2))  
+    contact_matrix[0,0] = default_contacts * (vax_popsize / popsize)
+    contact_matrix[0,1] = default_contacts * (unvax_popsize / popsize)
+    contact_matrix[1,0] = default_contacts * (vax_popsize / popsize)
+    contact_matrix[1,1] = default_contacts * (unvax_popsize / popsize)
+
+    base_config_vax['expected_contacts_per_day'] = contact_matrix[0,0]
+    base_config_unvax['expected_contacts_per_day'] = contact_matrix[1,1]
+
+    return [base_config_vax, base_config_unvax], [True, False], contact_matrix
