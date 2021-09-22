a0d177345dac677a13d4c4168002c694cea15d9b
Jiayue <wanjiayue.jason@gmail.com>
2021-09-22 15:52:21 -0400

    update code for generating the yaml files and update the params for spring 2021 student traj calibration

--- src/simulations_v2/params/spring_2021_calibration/set_calibration_params.py
+++ src/simulations_v2/params/spring_2021_calibration/set_calibration_params.py
@@ -1,32 +1,50 @@
 import yaml
+import numpy as np
+
 
 with open('nominal.yaml') as f:
     nominal_params = yaml.load(f, Loader=yaml.FullLoader)
 
 """
 parameter update history:
-[] test frequencies
-[] params for employees
-[x] others: last updated 9/20/2021
+[x] last updated 9/22/2021
 
 Group 1: UG, Greek/athlete
 Group 2: UG, non Greek/athlete
 Group 3: MBA
 Group 4: Grad/prof, non MBA
 """
+contact_matrix = np.array([[0.9318, 0.2004, 0.0000, 0.0232],
+    [0.0881, 0.2995, 0.0000, 0.0041],
+    [0.0000, 0.0000, 0.5000, 0.0265],
+    [0.0000, 0.0179, 0.0089, 0.0804]])
+interaction_matrix = np.transpose(contact_matrix)
+np.savetxt("interaction_matrix.csv", interaction_matrix)
+population_sizes = [3329, 9033, 534, 5227]
+arrival_positives = np.array([10, 49, 3, 7])
+outside_infection_rates = [1.44E-5, 1.77E-6, 7.49E-5, 1.53E-6]
+num_free_infectious_per_arrival_positive = 1.08
+secondary_infections_from_arrival_positives = np.dot(interaction_matrix, arrival_positives)
+total_num_free_infectious = arrival_positives * num_free_infectious_per_arrival_positive + \
+    secondary_infections_from_arrival_positives
+
+print("free and infectious:", arrival_positives * num_free_infectious_per_arrival_positive)
+print("secondary_infections:",  secondary_infections_from_arrival_positives)
+print("total free and infectious:", total_num_free_infectious)
+
 
 """
-private calibration for students
+Calibration for students
 group 1
 """
 params_group_1 = nominal_params.copy()
-params_group_1['population_size'] = 3329
+params_group_1['population_size'] = population_sizes[0]
 params_group_1['test_population_fraction'] = 3/7
-params_group_1['expected_contacts_per_day'] = 0.9464
+params_group_1['expected_contacts_per_day'] = float(contact_matrix[0,0])
 params_group_1['cases_isolated_per_contact'] = 0.8729
 params_group_1['cases_quarantined_per_contact'] = 3.11
-params_group_1['initial_ID_prevalence'] = 14.04 / params_group_1['population_size']
-params_group_1['daily_outside_infection_p'] = 1.44E-5
+params_group_1['initial_ID_prevalence'] = float(total_num_free_infectious[0] / params_group_1['population_size'])
+params_group_1['daily_outside_infection_p'] = outside_infection_rates[0]
 params_group_1['_scenario_name'] = 'Group 1 Students Parameters, Spring 2021'
 
 with open('group_1_students_spring_2021.yaml', 'w') as f:
@@ -34,15 +52,15 @@ with open('group_1_students_spring_2021.yaml', 'w') as f:
 
 
 """
-private calibration for students
+Calibration for students
 group 2
 """
 params_group_2 = params_group_1.copy()
-params_group_2['population_size'] = 9033
+params_group_2['population_size'] = population_sizes[1]
 params_group_2['test_population_fraction'] = 2/7
-params_group_2['expected_contacts_per_day'] = 0.3205
-params_group_2['initial_ID_prevalence'] = 56.16 / params_group_2['population_size']
-params_group_2['daily_outside_infection_p'] = 1.77E-6
+params_group_2['expected_contacts_per_day'] = float(contact_matrix[1,1])
+params_group_2['initial_ID_prevalence'] = float(total_num_free_infectious[1] / params_group_2['population_size'])
+params_group_2['daily_outside_infection_p'] = outside_infection_rates[1]
 params_group_2['_scenario_name'] = 'Group 2 Students Parameters, Spring 2021'
 
 with open('group_2_students_spring_2021.yaml', 'w') as f:
@@ -50,15 +68,15 @@ with open('group_2_students_spring_2021.yaml', 'w') as f:
 
 
 """
-private calibration for students
+Calibration for students
 group 3
 """
 params_group_3 = params_group_1.copy()
-params_group_3['population_size'] = 534
+params_group_3['population_size'] = population_sizes[2]
 params_group_3['test_population_fraction'] = 1/7
-params_group_3['expected_contacts_per_day'] = 0.5156
-params_group_3['initial_ID_prevalence'] = 3.24 / params_group_3['population_size']
-params_group_3['daily_outside_infection_p'] = 7.49E-5
+params_group_3['expected_contacts_per_day'] = float(contact_matrix[2,2])
+params_group_3['initial_ID_prevalence'] = float(total_num_free_infectious[2] / params_group_3['population_size'])
+params_group_3['daily_outside_infection_p'] = outside_infection_rates[2]
 params_group_3['_scenario_name'] = 'Group 3 Students Parameters, Spring 2021'
 
 with open('group_3_students_spring_2021.yaml', 'w') as f:
@@ -66,15 +84,15 @@ with open('group_3_students_spring_2021.yaml', 'w') as f:
 
 
 """
-private calibration for students
+Calibration for students
 group 4
 """
 params_group_4 = params_group_1.copy()
-params_group_4['population_size'] = 5227
+params_group_4['population_size'] = population_sizes[3]
 params_group_4['test_population_fraction'] = 1/7
-params_group_4['expected_contacts_per_day'] = 0.0849
-params_group_4['initial_ID_prevalence'] = 7.56 / params_group_4['population_size']
-params_group_4['daily_outside_infection_p'] = 1.53E-6
+params_group_4['expected_contacts_per_day'] = float(contact_matrix[3,3])
+params_group_4['initial_ID_prevalence'] = float(total_num_free_infectious[3] / params_group_4['population_size'])
+params_group_4['daily_outside_infection_p'] = outside_infection_rates[3]
 params_group_4['_scenario_name'] = 'Group 4 Students Parameters, Spring 2021'
 
 with open('group_4_students_spring_2021.yaml', 'w') as f:
