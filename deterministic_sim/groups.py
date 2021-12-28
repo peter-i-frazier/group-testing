fd1593091b6c9f03b018bfe23fac71f92fdf1718
samstan <sammsam1112@gmail.com>
2021-12-28 15:03:02 -0800

    ready to write test

--- deterministic_sim/groups.py
+++ deterministic_sim/groups.py
@@ -96,17 +96,22 @@ class population:
         Here, a "group" is an integer that corresponds to a meta-group and a group within that meta-group.
         '''
         dim_tot = 0 #total number of meta-group-groups
-        cum_tot = []
+        cum_tot = [] #help keep track of location in res matrix
         for i in meta_group_list:
             cum_tot.append(dim_tot)
             dim_tot += i.K
         
         res = np.zeros((dim_tot, dim_tot))
-        for i in range(len(meta_group_list)):
-            for j in range(meta_group_list[i].K):
-                for k in range(len(meta_group_list)):
-                    for l in range(meta_group_list[k].K):
-                        res=[]
+        for i in range(len(meta_group_list)): #source meta group
+            for j in range(meta_group_list[i].K): #source meta-group-group
+                for k in range(len(meta_group_list)): #exposed meta group
+                    for l in range(meta_group_list[k].K): #expoed meta-group-group
+                        q = meta_group_list[k].pop[l] * meta_group_list[k].contact_units[l] / np.sum(meta_group_list[k].pop * meta_group_list[k].contact_units)
+                        res[cum_tot[i]+j, cum_tot[k]+l] = \
+                        infections_per_contact_unit * meta_group_list[i].contact_units[j] * meta_group_contact_matrix[i,k] * q
+
+        return res
+        
     def idx_to_groupname(self, i):
         '''
         Returns a string naming the group indexed by i, e.g., "UG 6".
