823249fe92c9723ade8fc74e48fed0a253de05a3
Henry Robbins <hwr26@cornell.edu>
2021-12-28 15:00:18 -0500

    Added test_calibration with code for calibrating R0 and generation_time.

--- deterministic_sim/tests.py
+++ deterministic_sim/tests.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import micro
 from sim import sim, well_mixed_infection_rate
 from micro import __days_infectious_perfect_sensitivity__, days_infectious
@@ -29,7 +30,7 @@ def test_sim1():
 
     generation_time = 4/7 # in units of weeks
 
-    s = sim(T,S0,I0,R0,infection_rate,generation_time)
+    s = sim(T,S0,I0,R0,infection_rate=infection_rate,generation_time=generation_time)
     s.step(T-1)
 
     assert is_constant_population_size(s)
@@ -64,7 +65,9 @@ def test_sim4():
     infection_rate = well_mixed_infection_rate(pop, marginal_contacts, infections_per_contact)
     generation_time = 4/7 # in units of weeks
 
-    s = sim(T,S0,I0,R0,infection_rate,0,0,generation_time)
+    s = sim(T, S0, I0, R0, infection_rate=infection_rate, generation_time=generation_time,
+            infection_discovery_frac=0,
+            recovered_discovery_frac=0)
     s.step(T-1)
 
     assert is_constant_population_size(s)
@@ -136,7 +139,7 @@ def test_sim3():
 
     S0 = np.maximum(pop - R0 - I0, 0) # Need to take the max with 0 because R0[i]+S0[i] can be bigger than pop[i]
 
-    s = sim(T,S0,I0,R0,infection_rate,generation_time)
+    s = sim(T,S0,I0,R0,infection_rate=infection_rate, generation_time=generation_time)
     s.step(T-1)
 
     assert is_constant_population_size(s)
@@ -167,7 +170,7 @@ def test_sim2():
     infection_rate = well_mixed_infection_rate(pop, marginal_contacts, infections_per_contact)
     generation_time = 4/7 # in units of weeks
 
-    s = sim(T,S0,I0,R0,infection_rate,generation_time)
+    s = sim(T,S0,I0,R0,infection_rate=infection_rate,generation_time=generation_time)
     s.step(T-1)
 
     # The group with 0 contacts should not have any infections
@@ -250,7 +253,7 @@ def test_sim5():
     # 1x / week testing and 2 day delay
     infections_per_contact = booster_effectiveness * dec_infections_per_contact * micro.days_infectious(7,2) / dec_days_infectious
     infection_rate = well_mixed_infection_rate(pop, marginal_contacts, infections_per_contact)
-    s = sim(T, S0, I0, R0, infection_rate, generation_time)
+    s = sim(T, S0, I0, R0, infection_rate=infection_rate, generation_time=generation_time)
     s.step(T-1)
     plt.subplot(211)
     plt.plot(np.arange(T)*generation_time, s.get_discovered(aggregate=True,cumulative=True), label='1x/wk, 2d delay')
@@ -260,7 +263,7 @@ def test_sim5():
     # 2x / week testing and 1.5 day delay
     infections_per_contact = booster_effectiveness * dec_infections_per_contact * micro.days_infectious(3.5,1.5) / dec_days_infectious
     infection_rate = well_mixed_infection_rate(pop, marginal_contacts, infections_per_contact)
-    s = sim(T, S0, I0, R0, infection_rate, generation_time)
+    s = sim(T, S0, I0, R0, infection_rate=infection_rate, generation_time=generation_time)
     s.step(T-1)
     plt.subplot(211)
     plt.plot(np.arange(T)*generation_time, s.get_discovered(aggregate=True,cumulative=True), label='2x/wk, 1.5d delay')
@@ -270,7 +273,7 @@ def test_sim5():
     # 2x / week testing and 2 day delay
     infections_per_contact = booster_effectiveness * dec_infections_per_contact * micro.days_infectious(3.5,2) / dec_days_infectious
     infection_rate = well_mixed_infection_rate(pop, marginal_contacts, infections_per_contact)
-    s = sim(T, S0, I0, R0, infection_rate, generation_time)
+    s = sim(T, S0, I0, R0, infection_rate=infection_rate, generation_time=generation_time)
     s.step(T-1)
     plt.subplot(211)
     plt.plot(np.arange(T)*generation_time, s.get_discovered(aggregate=True,cumulative=True), label='2x/wk, 2d delay')
@@ -280,7 +283,7 @@ def test_sim5():
     # 2x / week testing and 1 day delay
     infections_per_contact = booster_effectiveness * dec_infections_per_contact * micro.days_infectious(3.5,1) / dec_days_infectious
     infection_rate = well_mixed_infection_rate(pop, marginal_contacts, infections_per_contact)
-    s = sim(T, S0, I0, R0, infection_rate, generation_time)
+    s = sim(T, S0, I0, R0, infection_rate=infection_rate, generation_time=generation_time)
     s.step(T-1)
     plt.subplot(211)
     plt.plot(np.arange(T)*generation_time, s.get_discovered(aggregate=True,cumulative=True), label='2x/wk, 1d delay')
@@ -290,7 +293,7 @@ def test_sim5():
     # 1x / week testing and 1 day delay
     infections_per_contact = booster_effectiveness * dec_infections_per_contact * micro.days_infectious(7,1) / dec_days_infectious
     infection_rate = well_mixed_infection_rate(pop, marginal_contacts, infections_per_contact)
-    s = sim(T,S0,I0,R0,infection_rate,generation_time)
+    s = sim(T, S0, I0, R0, infection_rate=infection_rate, generation_time=generation_time)
     s.step(T-1)
     plt.subplot(211)
     plt.plot(np.arange(T)*generation_time, s.get_discovered(aggregate=True,cumulative=True), label='1x/wk, 1d delay')
@@ -300,7 +303,7 @@ def test_sim5():
     # 1x / week testing and 1.5 day delay
     infections_per_contact = booster_effectiveness * dec_infections_per_contact * micro.days_infectious(7,1.5) / dec_days_infectious
     infection_rate = well_mixed_infection_rate(pop, marginal_contacts, infections_per_contact)
-    s = sim(T,S0,I0,R0,infection_rate,generation_time)
+    s = sim(T, S0, I0, R0, infection_rate=infection_rate, generation_time=generation_time)
     s.step(T-1)
     plt.subplot(211)
     plt.plot(np.arange(T)*generation_time, s.get_discovered(aggregate=True,cumulative=True), label='1x/wk, 1.5d delay')
@@ -312,7 +315,9 @@ def test_sim5():
     infection_rate = well_mixed_infection_rate(pop, marginal_contacts, infections_per_contact)
     infection_discovery_frac = symptomatic_rate # 30% are asymptomatic
     recovered_discovery_frac = .01 # 1% of the population is tested for any reason in a given generation
-    s = sim(T,S0,I0,R0,infection_rate,generation_time,infection_discovery_frac,recovered_discovery_frac)
+    s = sim(T, S0, I0, R0, infection_rate=infection_rate, generation_time=generation_time,
+            infection_discovery_frac=infection_discovery_frac,
+            recovered_discovery_frac=recovered_discovery_frac)
     s.step(T-1)
     plt.subplot(211)
     plt.plot(np.arange(T)*generation_time, s.get_discovered(aggregate=True,cumulative=True), 'k-', label='No surveillance, Discovered')
@@ -335,6 +340,89 @@ def test_sim5():
     plt.ylabel('UG in Isolation')
 
     plt.savefig('test_sim5.png', facecolor='w')
+    plt.close()
+
+
+def test_sim_calibration():
+    total_pop = 16000 # total UG population
+    K = 12 # number of distinct contact groups
+    T = 5 # num generations
+    initial_group = 11
+
+    # Based on Xiangyu's adjusted moment match code, this is the fraction of the
+    # population (UG-only?  or all students) broken out by the amount of contact
+    # that they have, starting from low contact to high contact.
+    pop_frac = np.array(
+        [0.46666204134859246,
+         0.21110377424326393,
+         0.13427835918082992,
+         0.040993148549700376,
+         0.05561449714374835,
+         0.03772148571886847,
+         0.020557396664413034,
+         0.01829685176380435,
+         0.003308325172067398,
+         0.006056046991853723,
+         0.0027991704708900224,
+         0.002608902751968122])
+    pop = total_pop * pop_frac
+
+    marginal_contacts = np.arange(1,K+1)
+
+    # Assume no recovered and 1 infected with Omicron
+    R0 = np.zeros(K)
+    I0 = np.zeros(K)
+    I0[initial_group] = 1
+
+    # Need to take the max with 0 because R0[i]+S0[i] can be bigger than pop[i]
+    S0 = np.maximum(pop - R0 - I0, 0)
+
+    generation_time = 4 # days
+    symptomatic_rate = .3 # fraction of new infections will be discovered without surevillance
+
+    # We calibrate our probability of infection to the December Omicron outbreak
+    # We use a ballpark estimate of the effective R0 during that period, and an estimate of which group was the most
+    # important one in driving that outbreak.  We then assume that the probability of infection is such that this
+    # group's effective R0 (under the testing intervention at the time) was equal to the ballpark estimate.
+    # Assuming a generation time of 4 days, and 50% day-over-day growth, we get a ballpark estimate of 1.5^4 = 5.06
+    # for the december effective R0
+    dec_effective_R0 = 24
+    dec_contacts_of_key_group = 6
+    dec_infections_per_contact = dec_effective_R0 / dec_contacts_of_key_group
+    dec_days_infectious = micro.days_infectious(7,2)
+
+    # We believe that boosters reduce the transmission of virus by a factor of 2
+    booster_effectiveness = 0.5
+
+    # [12/1 to 12/9] 1x / week testing with 36hr delay
+    infections_per_contact = booster_effectiveness * dec_infections_per_contact * micro.days_infectious(7,1.5) / dec_days_infectious
+    infection_rate = well_mixed_infection_rate(pop, marginal_contacts, infections_per_contact)
+    s = sim(T, S0, I0, R0, infection_rate=infection_rate, generation_time=generation_time)
+    s.step(2)
+
+    # [12/10 to 12/16] 1x / week testing with 3 day delay
+    infections_per_contact = booster_effectiveness * dec_infections_per_contact * micro.days_infectious(7,3) / dec_days_infectious
+    infection_rate = well_mixed_infection_rate(pop, 0.5*marginal_contacts, infections_per_contact)
+    s.step(1, infection_rate=infection_rate)
+
+    infections_per_contact = booster_effectiveness * dec_infections_per_contact * micro.days_infectious(7,3) / dec_days_infectious
+    infection_rate = well_mixed_infection_rate(pop, 0.33*marginal_contacts, infections_per_contact)
+    s.step(1, infection_rate=infection_rate)
+
+    I = s.get_metric('I', aggregate=True, cumulative=True)
+    plt.plot(np.arange(T)*generation_time, I, label="model I")
+
+    # Plot actual counts
+    dec_daily_positives = list(pd.read_csv("data/dec_infections.csv")['positives'])
+    dec_positives = np.cumsum(dec_daily_positives)
+    plt.plot(np.arange(15), dec_positives[:15], label="actual")
+
+    plt.title(f"Dec Effective R0 = {dec_effective_R0}, Symptomatic Rate = {symptomatic_rate}")
+    plt.rcParams.update({'font.size': 8})
+    plt.xlabel('Days Since Dec. 1')
+    plt.ylabel('Cumulative Infected')
+    plt.legend()
+    plt.savefig('test_sim6.png', facecolor='w')
     plt.close()
 
 
