685f70ee394ad186e920b41aab96358512f100f0
Henry Robbins <hwr26@cornell.edu>
2021-12-30 09:40:05 -0500

    Cleaned up plotting.plot_comprehensive_summary

--- deterministic_sim/plotting.py
+++ deterministic_sim/plotting.py
@@ -70,54 +70,48 @@ def plot_comprehensive_summary(s: sim, pop: population, params):
         pop (population): Population used in the simulation.
         params ([type]): Parameters used in the simulation.
     """
+    plt.figure(figsize=(18, 12), dpi=300)
     X = np.arange(s.max_T)*s.generation_time
 
-    plt.figure(figsize=(18, 12), dpi=300)
+    # plot infectious / discovered for each meta-group
     groups = pop.metagroup_indices(params["population_names"])
     for i in range(4):
+        infectious = s.get_total_infected_for_different_groups(groups[i], cumulative=True)
+        discovered = s.get_total_discovered_for_different_groups(groups[i], cumulative=True)
+        infectious_lbl = "Infected: " + params["population_names"][i]
+        discovered_lbl = "Discovered: " + params["population_names"][i]
         plt.subplot("24" + str(i + 1))
-        plt.plot(X,
-                s.get_total_infected_for_different_groups(groups[i], cumulative=True),
-                'k--', label="Infected: " + params["population_names"][i], color='r')
-        plt.plot(X,
-                s.get_total_discovered_for_different_groups(groups[i], cumulative=True),
-                'k--', label="Discovered: " + params["population_names"][i], color='blue')
-
+        plt.plot(X, infectious, 'k--', label=infectious_lbl, color='r')
+        plt.plot(X, discovered, 'k--', label=discovered_lbl, color='blue')
         plt.title("No surveillance")
         plt.legend()
 
-    # for i in range(4):
-    #     plt.subplot("24" + str(i))
-    #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_discovered(aggregate=True,cumulative=True), 'k-', label='No surveillance, Discovered')
-    #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_infected(aggregate=True,cumulative=True), 'k--', label='No surveillance, Infected')
-    # # Iterate over meta-groups, plot infections for each
-    # for i in range(4): # replace by number of meta-groups
-    #     # Do this for the meta-group rather than for everyone
-    #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_discovered(aggregate=True,cumulative=True), 'k-', label='No surveillance, Discovered')
-    #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_infected(aggregate=True,cumulative=True), 'k--', label='No surveillance, Infected')
-
+    # TODO: Weighted sum of infections across meta-groups that is intended to
+    # be hospitalizations, where the employee infections result in more
+    # hospitalizations per infection because we are older
     plt.subplot(245)
-    # Peter
-    # Some weighted sum of infections across meta-groups that is intended to be hospitalizations, where the employee infections result in more hospitalizations per infection because we are older
 
+
+    # TODO: On-campus UG + grad-research + grad-professional in isolation
+    # (since this determines our housing needs)
+    # NOTE: this is placeholder code
     plt.subplot(246)
-    # Peter
-    # On-campus UG + grad-research + grad-professional in isolation (since this determines our housing needs)
     isolated = s.get_isolated(iso_lengths=params["isolation_durations"],
-                            iso_props=params["isolation_fracs"],
-                            on_campus_frac=params["on_campus_frac"])
+                              iso_props=params["isolation_fracs"],
+                              on_campus_frac=params["on_campus_frac"])
     plt.plot(X, isolated, 'k', label='No surveillance')
 
+    # TODO: All UG + grad-professional in isolation
+    # NOTE: this is placeholder code
     plt.subplot(247)
-    # All UG + grad-professional in isolation
-    # Peter
     isolated = s.get_isolated(iso_lengths=params["isolation_durations"],
-                            iso_props=params["isolation_fracs"],
-                            on_campus_frac=params["on_campus_frac"])
+                              iso_props=params["isolation_fracs"],
+                              on_campus_frac=params["on_campus_frac"])
     plt.plot(X, isolated, 'k', label='No surveillance')
 
+    # TODO: Some text that includes all of the parameters that were used,
+    # the github commit, and a timestamp
     plt.subplot(248)
-    # Some text that includes all of the parameters that were used, the github commit, and a timestamp
     # plt.text()
 
     plt.savefig('sp22_sim_summary.png', facecolor='w')
