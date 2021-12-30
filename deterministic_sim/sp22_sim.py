a947805ce2e4b469a521a29853401d9451f9f24c
Henry Robbins <hwr26@cornell.edu>
2021-12-30 18:32:30 -0500

    Have sp22_sim weight initial SIR on contacts (instead of population).

--- deterministic_sim/sp22_sim.py
+++ deterministic_sim/sp22_sim.py
@@ -53,7 +53,8 @@ def main(**kwargs):
 
 
     popul = population(meta_groups, np.array(params['meta_matrix']))
-    S0, I0, R0 = popul.get_init_SIR_vec(initial_infections, past_infections)
+    S0, I0, R0 = popul.get_init_SIR_vec(initial_infections, past_infections,
+                                        weight="contacts")
     outside_rates = params['outside_rates']
     outside_rate = popul.get_outside_rate(outside_rates)
 
