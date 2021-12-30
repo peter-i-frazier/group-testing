63ee6aa1457a239aaf3b3f83c636d641cccab848
Peter Frazier <peter.i.frazier@gmail.com>
2021-12-29 22:43:27 -0500

    Attempts to simplify how we pass infectivity parameters (contact units, infections per contact unit). Also, extending the code to be able to handle different amounts of past and active infections by meta-group.

--- deterministic_sim/sp22_sim.py
+++ deterministic_sim/sp22_sim.py
@@ -27,18 +27,18 @@ def main(**kwargs):
     SYMPTOMATIC_RATE = params['symptomatic_rate']
     R0_REDUCTION = params['R0_reduction']
     BOOSTER_EFFECTIVENESS = params['booster_effectiveness']
-    DEC_INFECTIONS_PER_CONTACT = params['dec_effective_R0'] / \
-                                 params['dec_contacts_of_key_group']
-    DEC_DAYS_INFECTIOUS = micro.days_infectious(params['dec_days_between_tests'],
-                                                params['dec_isolation_delay'])
+    DEC_UG_INFECTED_PER_DAY_UNIT = params['dec_ug_infected_per_day_unit']
 
     # =====================================================================
     # [Initialize] Assume a group's previous and new infections are divided
     # proportionally to the amount of contact it has as a group.
     # =====================================================================
 
+
     population_count = params["population_count"]
     population_names = params["population_names"]
+    initial_infections = params['initial_infections']
+    past_infections = params['past_infections']
     meta_groups = []
     for i in range(len(population_count)):
         name = population_names[i]
@@ -46,11 +46,15 @@ def main(**kwargs):
         contact_units = np.arange(1, len(pop) + 1)
         meta_groups.append(meta_group(name, pop, contact_units))
 
+    # TODO this needs to be updated to use the new yaml, which stores these as a vector
+    # TODO Xiangyu was going to do this
     initial_infectious = params['initial_infections']
     initial_recovered = params['infected_from_outbreak'] + params['infected_over_break']
 
     popul = population(meta_groups, np.array(params['meta_matrix']))
-    S0, I0, R0 = popul.get_init_SIR(initial_infectious, initial_recovered)
+    S0, I0, R0 = popul.get_init_SIR(initial_infectious, initial_recovered) # TODO update as part of the change a few lines above
+
+    population_contact_units = params['contact_units']
 
     # ========================================
     # [Run] Reduce R0 once the semester begins
@@ -58,8 +62,8 @@ def main(**kwargs):
 
     def sim_test_regime(tests_per_week, delay, color):
         days_between_tests = 7 / tests_per_week
-        infections_per_contact = BOOSTER_EFFECTIVENESS * DEC_INFECTIONS_PER_CONTACT * micro.days_infectious(days_between_tests, delay) / DEC_DAYS_INFECTIOUS
-        infection_rate = popul.infection_matrix(infections_per_contact)
+        ug_infections_per_contact_unit = BOOSTER_EFFECTIVENESS * DEC_UG_INFECTED_PER_DAY_UNIT * micro.days_infectious(days_between_tests, delay)
+        infection_rate = popul.infection_matrix(ug_infections_per_contact_unit * population_contact_units)
         s = sim(T, S0, I0, R0, infection_rate=infection_rate,
                 generation_time=GENERATION_TIME)
         s.step(4)
