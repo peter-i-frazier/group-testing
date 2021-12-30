bcb0874e60ef875d34f6ed72a48e74311e186123
Henry Robbins <hwr26@cornell.edu>
2021-12-30 00:31:31 -0500

    Uncommented old plottting code and let user choose either.

--- deterministic_sim/sp22_sim.py
+++ deterministic_sim/sp22_sim.py
@@ -91,74 +91,78 @@ def main(**kwargs):
     s.step(4)
     s.step(T-1-4, infection_rate=R0_REDUCTION * infection_rate)
 
-
-    groups = popul.metagroup_indices(params["population_names"])
-    for i in range(4):
-        plt.subplot("24" + str(i + 1))
-        plt.plot(np.arange(T)*GENERATION_TIME,
-                 s.get_total_infected_for_different_groups(groups[i], cumulative=True),
-                 'k--', label="Infected: " + params["population_names"][i], color='r')
-        plt.plot(np.arange(T)*GENERATION_TIME,
-                 s.get_total_discovered_for_different_groups(groups[i], cumulative=True),
-                 'k--', label="Discovered: " + params["population_names"][i], color='blue')
-
-        plt.title("No surveillance")
-        plt.legend()
-
-
-    # for i in range(4):
-    #     plt.subplot("24" + str(i))
-    #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_discovered(aggregate=True,cumulative=True), 'k-', label='No surveillance, Discovered')
-    #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_infected(aggregate=True,cumulative=True), 'k--', label='No surveillance, Infected')
-    # # Iterate over meta-groups, plot infections for each
-    # for i in range(4): # replace by number of meta-groups
-    #     # Do this for the meta-group rather than for everyone
-    #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_discovered(aggregate=True,cumulative=True), 'k-', label='No surveillance, Discovered')
-    #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_infected(aggregate=True,cumulative=True), 'k--', label='No surveillance, Infected')
-
-    plt.subplot(245)
-    # Peter
-    # Some weighted sum of infections across meta-groups that is intended to be hospitalizations, where the employee infections result in more hospitalizations per infection because we are older
-
-    plt.subplot(246)
-    # Peter
-    # On-campus UG + grad-research + grad-professional in isolation (since this determines our housing needs)
-    isolated = s.get_isolated(iso_lengths=params["isolation_durations"],
-                              iso_props=params["isolation_fracs"],
-                              on_campus_frac=params["on_campus_frac"])
-    plt.plot(np.arange(T) * GENERATION_TIME, isolated, 'k', label='No surveillance')
-
-    plt.subplot(247)
-    # All UG + grad-professional in isolation
-    # Peter
-    isolated = s.get_isolated(iso_lengths=params["isolation_durations"],
-                              iso_props=params["isolation_fracs"],
-                              on_campus_frac=params["on_campus_frac"])
-    plt.plot(np.arange(T) * GENERATION_TIME, isolated, 'k', label='No surveillance')
-
-    plt.subplot(248)
-    # Some text that includes all of the parameters that were used, the github commit, and a timestamp
-    # plt.text()
-
     # ====================================
     # [Plot] Comparison of testing regimes
     # ====================================
 
-    # @peter: Xiangyu commented the following out
-    # plt.subplot(211)
-    # plt.title(f"Dec Effective R0 = {params['dec_effective_R0']}, Symptomatic Rate = {SYMPTOMATIC_RATE}")
-    # plt.rcParams.update({'font.size': 8})
-    # plt.legend()
-    # plt.ylabel('UG Infected')
+    def old_plot():
+        plt.subplot(211)
+        plt.title(f"dec_ug_infected_per_day_unit={DEC_UG_INFECTED_PER_DAY_UNIT}, Symptomatic Rate = {SYMPTOMATIC_RATE}")
+        plt.rcParams.update({'font.size': 8})
+        plt.legend()
+        plt.ylabel('UG Infected')
 
-    # plt.subplot(212)
-    # plt.rcParams.update({'font.size': 8})
-    # plt.legend()
-    # plt.xlabel('Weeks')
-    # plt.ylabel('UG in Isolation (on-campus 5day)')
+        plt.subplot(212)
+        plt.rcParams.update({'font.size': 8})
+        plt.legend()
+        plt.xlabel('Weeks')
+        plt.ylabel('UG in Isolation (on-campus 5day)')
 
-    plt.savefig('sp22_sim_xz.png', facecolor='w')
-    plt.close()
+        plt.savefig('sp22_sim.png', facecolor='w')
+
+    def new_plot():
+        groups = popul.metagroup_indices(params["population_names"])
+        for i in range(4):
+            plt.subplot("24" + str(i + 1))
+            plt.plot(np.arange(T)*GENERATION_TIME,
+                    s.get_total_infected_for_different_groups(groups[i], cumulative=True),
+                    'k--', label="Infected: " + params["population_names"][i], color='r')
+            plt.plot(np.arange(T)*GENERATION_TIME,
+                    s.get_total_discovered_for_different_groups(groups[i], cumulative=True),
+                    'k--', label="Discovered: " + params["population_names"][i], color='blue')
+
+            plt.title("No surveillance")
+            plt.legend()
+
+
+        # for i in range(4):
+        #     plt.subplot("24" + str(i))
+        #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_discovered(aggregate=True,cumulative=True), 'k-', label='No surveillance, Discovered')
+        #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_infected(aggregate=True,cumulative=True), 'k--', label='No surveillance, Infected')
+        # # Iterate over meta-groups, plot infections for each
+        # for i in range(4): # replace by number of meta-groups
+        #     # Do this for the meta-group rather than for everyone
+        #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_discovered(aggregate=True,cumulative=True), 'k-', label='No surveillance, Discovered')
+        #     plt.plot(np.arange(T)*GENERATION_TIME, s.get_infected(aggregate=True,cumulative=True), 'k--', label='No surveillance, Infected')
+
+        plt.subplot(245)
+        # Peter
+        # Some weighted sum of infections across meta-groups that is intended to be hospitalizations, where the employee infections result in more hospitalizations per infection because we are older
+
+        plt.subplot(246)
+        # Peter
+        # On-campus UG + grad-research + grad-professional in isolation (since this determines our housing needs)
+        isolated = s.get_isolated(iso_lengths=params["isolation_durations"],
+                                iso_props=params["isolation_fracs"],
+                                on_campus_frac=params["on_campus_frac"])
+        plt.plot(np.arange(T) * GENERATION_TIME, isolated, 'k', label='No surveillance')
+
+        plt.subplot(247)
+        # All UG + grad-professional in isolation
+        # Peter
+        isolated = s.get_isolated(iso_lengths=params["isolation_durations"],
+                                iso_props=params["isolation_fracs"],
+                                on_campus_frac=params["on_campus_frac"])
+        plt.plot(np.arange(T) * GENERATION_TIME, isolated, 'k', label='No surveillance')
+
+        plt.subplot(248)
+        # Some text that includes all of the parameters that were used, the github commit, and a timestamp
+        # plt.text()
+
+        plt.savefig('sp22_sim_xz.png', facecolor='w')
+
+
+    new_plot()
 
 
 if __name__ == "__main__":
